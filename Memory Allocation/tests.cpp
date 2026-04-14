8a07624f10ff4e0eda3dc8f128d59042e1526baa
Jaico700 <akarnav202@gmail.com>
2026-04-14 10:27:32 +0530

    new file

--- Memory Allocation/tests.cpp
+++ Memory Allocation/tests.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
+
+int *update(int *arr,int n, int data)
+{
+    for(int i = 0; i < n; i++)
+    {
+        *(arr + i) = data;
+    }
+    return arr;
+}
+
 int main()
 {
-    while(true)
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
     {
-        int *i = new int;
+        cin >> *(arr + i);
     }
+
+    int *newarr = update(arr ,n, 10);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << *(arr + i) << " ";
+    }
+    delete[] arr;
+    return 0; 
 }
